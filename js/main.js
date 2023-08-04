a9a59484376c38295a3a095969bfd9e120e2a46b
normurodv <normurodovnurbek0018@gmail.com>
2023-08-04 17:07:00 +1200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let name=prompt("birinchi sonni kiriting: ")
+let surname=prompt("ikkinchi sonni kiriting: ")
+
+console.log(alert(Number(name)+ Number(surname)));
+console.log(alert(Number(name)- Number(surname)));
+console.log(alert(Number(name)* Number(surname)));
+console.log(alert(Number(name)/ Number(surname)));
